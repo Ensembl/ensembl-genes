3a26b0d271a4d5022a2657bf2d7987e43f58b883
ens-carlos <ens-carlos@users.noreply.github.com>
2020-10-07 16:20:12 +0100

    Undo: added the relative path to pylintrc. Didn't work.

--- scripts/travis/pylint_harness.sh
+++ scripts/travis/pylint_harness.sh
@@ -17,7 +17,7 @@
 
 disabled="--disable=max-line-length,similarities,invalid-name,too-many-statements,too-many-arguments,too-many-locals,too-few-public-methods,relative-import,no-self-use"
 
-pylint ${disabled} --rcfile ensembl_genes/pylintrc ensembl_genes/ > output.err
+pylint ${disabled} --rcfile pylintrc ensembl_genes/ > output.err
 
 grep -v "\-\-\-\-\-\-\-\-\-" output.err | grep -v "Your code has been rated" | grep -v "\n\n" | sed '/^$/d' > pylint.err
 
