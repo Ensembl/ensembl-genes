0b632f40c2cbfd3f28b9c61d87d1ffa983e8578b
ens-carlos <ens-carlos@users.noreply.github.com>
2020-10-07 16:20:53 +0100

    Added wrong-import-order to disable in pylint harness.

--- scripts/travis/pylint_harness.sh
+++ scripts/travis/pylint_harness.sh
@@ -15,7 +15,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-disabled="--disable=max-line-length,similarities,invalid-name,too-many-statements,too-many-arguments,too-many-locals,too-few-public-methods,relative-import,no-self-use"
+disabled="--disable=wrong-import-order,max-line-length,similarities,invalid-name,too-many-statements,too-many-arguments,too-many-locals,too-few-public-methods,relative-import,no-self-use"
 
 pylint ${disabled} --rcfile pylintrc ensembl_genes/ > output.err
 
