8e85b506aae04591197f9125003d0baeefa2189c
ens-carlos <ens-carlos@users.noreply.github.com>
2020-10-08 09:01:45 +0100

    Removed unnecessary max-line-length in pylint harness disable.

--- scripts/travis/pylint_harness.sh
+++ scripts/travis/pylint_harness.sh
@@ -15,7 +15,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-disabled="--disable=line-too-long,wrong-import-order,max-line-length,similarities,invalid-name,too-many-statements,too-many-arguments,too-many-locals,too-few-public-methods,relative-import,no-self-use"
+disabled="--disable=line-too-long,wrong-import-order,similarities,invalid-name,too-many-statements,too-many-arguments,too-many-locals,too-few-public-methods,relative-import,no-self-use"
 
 pylint ${disabled} --rcfile pylintrc ensembl_genes/ > output.err
 
