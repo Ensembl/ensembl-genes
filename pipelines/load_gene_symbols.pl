27c1cc27b1abd10d5764addd124ac5c1c95cbee9
William Stark <william@ebi.ac.uk>
2021-09-10 16:20:55 +0100

    probably complete, some cleanup needed

--- pipelines/load_gene_symbols.pl
+++ pipelines/load_gene_symbols.pl
@@ -41,6 +41,7 @@ use Getopt::Long;
 
 use Data::Dump qw(dump);
 
+use Bio::EnsEMBL::DBEntry;
 use Bio::EnsEMBL::DBSQL::DBAdaptor;
 
 
@@ -63,7 +64,7 @@ my $options = GetOptions (
     "db_name=s" => \$db_name,
     "username=s" => \$username,
     "password=s" => \$password,
-    "symbol_assignments=s" => \$symbol_assignments
+    "symbol_assignments=s" => \$symbol_assignments,
 );
 
 
@@ -73,28 +74,29 @@ my $db = new Bio::EnsEMBL::DBSQL::DBAdaptor(
     -port   => $db_port,
     -user   => $username,
     -pass   => $password,
-    -dbname => $db_name
+    -dbname => $db_name,
 );
 
 
-# generate a gene adaptor for the database
+# generate a gene adaptor
 my $gene_adaptor = $db->get_GeneAdaptor();
 
+# generate a DBEntry adaptor for Xref
+my $xref_adaptor = $db->get_DBEntryAdaptor();
 
 # open the gene symbol assignments TSV file
-open(my $symbol_assignments_file, "<", $symbol_assignments);
+open(my $symbol_assignments_file, "<", $symbol_assignments) or die "Could not open file: $!";
+
+my $counter = 0;
 
 # read gene symbol assignments from the TSV file and load them to the core database
 while (my $line = <$symbol_assignments_file>) {
-    print("\n");
-
     # remove newline (line feed and carriage return) from the line
     $line =~ s/\n//g;
     $line =~ s/\r//g;
 
     # get TSV field values
     my @fields = split(/\t/, $line);
-    #dump(@fields);
     my $stable_id = $fields[0];
     my $symbol = $fields[1];
     my $probability = $fields[2];
@@ -104,22 +106,62 @@ while (my $line = <$symbol_assignments_file>) {
         next;
     }
 
+    #$stable_id = "ENSBMSG00020000022.1";
+    #$stable_id = "ENSBMSG00000000000.0";
+
     # generate gene object for gene stable_id
     my $gene = $gene_adaptor->fetch_by_stable_id($stable_id);
 
-    # skip gene if display_xref is already defined
-    if ($gene->display_xref()) {
-      next;
+    my $display_xref = $gene->display_xref();
+
+    # skip gene if display_xref is already set
+    if ($display_xref) {
+        say("Xref exists for gene ".$stable_id.", symbol: ".$display_xref->display_id().", skipping.");
+        next;
+        #exit;
     }
+
+    # use the symbol for probability >= 0.9, a tilde for a "like" symbol for probability >= 0.7,
+    # skip assignments with lower probability
+    my $display_id;
+    if ($probability >= 0.9) {
+        $display_id = $symbol;
+    } elsif ($probability >= 0.7) {
+        $display_id = "~".$symbol;
+    } else {
+        next;
+    }
+
+    # generate xref object
+    my $confidence = 100 * sprintf("%.4f", $probability);
+    my $xref_description = "Ensembl Gene Symbol Classifier assignment ".$symbol." with ".$confidence."% confidence";
+    my $xref_object = Bio::EnsEMBL::DBEntry->new(
+        -primary_id => $display_id,
+        -display_id => $display_id,
+        -dbname => "HGNC",
+        -description => $xref_description,
+    );
 
     dump($stable_id);
     dump($symbol);
     dump($probability);
+    dump($display_id);
+    dump($xref_description);
 
-    my $display_xref = $gene->display_xref();
-    dump($display_xref);
+    $xref_adaptor->store($xref_object, $gene->dbID, "Gene", 1);
 
-    last;
+    $gene->display_xref($xref_object);
+
+    $gene_adaptor->update($gene);
+
+    print("\n");
+
+    $counter++;
+    if ($counter == 3) {
+        exit;
+    }
+
+    #exit;
 }
 
 close $symbol_assignments_file;
