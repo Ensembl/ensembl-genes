cb09ccfe8a9d2149594a131582265c1f1f951381
William Stark <william@ebi.ac.uk>
2022-11-10 16:39:04 +0000

    round probability to 4 decimal places

--- pipelines/gene_symbol_classifier/filter_assignments.py
+++ pipelines/gene_symbol_classifier/filter_assignments.py
@@ -47,7 +47,11 @@ def filter_assignments(symbol_assignments, threshold):
 
     all_symbols = pd.read_csv(gene_symbols_csv_path, sep="\t")
 
+    # select assignments with probability above threshold
     filtered_assignments = all_symbols.loc[all_symbols["probability"] >= threshold]
+
+    # round probability column to 4 decimal places
+    filtered_assignments = filtered_assignments.round({"probability": 4})
 
     filtered_symbols_csv_path = pathlib.Path(
         f"{gene_symbols_csv_path.parent}/{gene_symbols_csv_path.stem}_filtered.csv"
