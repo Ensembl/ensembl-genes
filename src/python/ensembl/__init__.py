950b0195ddd2f5714b73afe3e814944ea4671997
‘ens-ftricomi’ <ftricomi@ebi.ac.uk>
2025-11-28 16:52:31 +0000

    Add minimal src/python/ensembl/__init__.py to make package importable for docs generation

---
+++ src/python/ensembl/__init__.py
@@ -0,0 +1,7 @@
+"""Top-level ensembl package for ensembl-genes documentation imports.
+
+This file intentionally kept minimal to allow `mkdocstrings` to import
+subpackages under `src/python/ensembl` without executing package-level code.
+"""
+
+__all__ = []
